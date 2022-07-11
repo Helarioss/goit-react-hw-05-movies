ff02548095e88562754f256ff01f6dcf22f73b19
Helarioss <53579288+Helarioss@users.noreply.github.com>
2022-07-11 16:09:01 +0000

    Deploying to gh-pages from @ Helarioss/goit-react-hw-05-movies@265d90fdf0777f357d0f671b5c72441d810d115b 🚀

---
+++ static/js/148.67eeb1f1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[148],{148:function(n,e,t){t.r(e),t.d(e,{default:function(){return o}});var i,r=t(168),c=t(6031).ZP.h1(i||(i=(0,r.Z)(["\n  height: 100%;\n\n  display: flex;\n  align-items: center;\n  justify-content: center;\n"]))),u=t(184),o=function(){return(0,u.jsx)(c,{children:"Sorry. Page not found"})}}}]);
+//# sourceMappingURL=148.67eeb1f1.chunk.js.map
